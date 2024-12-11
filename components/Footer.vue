6d51058666c185ff39d8c58a7f083397309b2247
Realgreat <samsonrealgreat@gmail.com>
2024-12-11 07:32:08 +0100

    fixed the navbar and mobile responsiveness

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-  <UContainer class="grid bg-white lg:grid-cols-2">
-    <div class="flex w-3/4 flex-col justify-center gap-y-10">
-      <h2 class="text-2xl font-semibold">Join the Waitlist and Earn More</h2>
-      <p class="text-xl">
+  <UContainer class="grid bg-white py-10 lg:grid-cols-2 lg:py-0">
+    <div class="flex flex-col justify-center gap-y-4 lg:w-3/4 lg:gap-y-10">
+      <h2 class="text-xl font-semibold lg:text-2xl">
+        Join the Waitlist and Earn More
+      </h2>
+      <p class="text-base lg:text-xl">
         Don’t just process payments—turn every transaction into an opportunity.
         Greeppay gives you seamless payment solutions and rewards your growth
         with GRP tokens.
